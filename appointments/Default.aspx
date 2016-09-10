4d6e2188b54fae058f7065365146606dcb133a82
Carter Sills <csills2313@gmail.com>
2016-09-10 13:41:33 -0400

    Initial Upload
    Initial Upload to repository

---
+++ appointments/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="appointments.Default" %>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <!-- jQuery, jQuery.ui -->
+    <link href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    <!-- React -->
+    <script src="https://fb.me/react-0.13.3.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.29/browser.js"></script>
+
+    <!--Custom Styles -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <title>Appointments List</title>
+</head>
+<body>
+    <div id="react-container" align="center"></div>
+    <script src="Scripts/appointments.js" type="text/babel" ></script>
+</body>
+</html>
---
+++ appointments/Scripts/appointments.js
@@ -0,0 +1,144 @@
+﻿var Note = React.createClass({
+    getInitialState: function() {
+        return {editing: false}
+        
+        
+    },
+    edit: function() {
+        this.setState({editing: true});
+        return{saving: false}
+    },
+    save: function() {
+        this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);       
+        this.setState({saving: true});
+        this.setState({editing: false});
+       
+        
+    },
+    remove: function() {
+        this.props.onRemove(this.props.index);
+    },
+    renderDisplay: function() {
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
+                <span>
+                    <button onClick={this.edit}
+        className="btn btn-primary glyphicon glyphicon-pencil"/>
+<button onClick={this.remove}
+        className="btn btn-danger glyphicon glyphicon-trash"/>
+</span>
+</div>
+            );
+    },
+
+    renderDisplay2: function() {
+        return (
+            <div className="notered">
+                <p>{this.props.children}</p>
+                <span>
+                    <button onClick={this.edit}
+        className="btn btn-primary glyphicon glyphicon-pencil"/>
+<button onClick={this.remove}
+        className="btn btn-danger glyphicon glyphicon-trash"/>
+</span>
+</div>
+           );
+    },
+    renderForm: function() {
+        return (
+            <div className="note">
+            <textarea ref="newText" defaultValue={this.props.children} 
+        className="form-control"></textarea>
+        <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+        </div>
+            )
+    },
+    renderForm2: function() {
+        return (
+            <div className="notered">
+            <textarea ref="newText" defaultValue={this.props.children} 
+        className="form-control"></textarea>
+        <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+        </div>
+            )
+    },
+    render: function() {
+        if (this.state.editing) {
+           
+            return this.renderForm();
+        }
+        else if (this.state.saving) {
+            return this.renderDisplay2();
+        }
+        else {
+
+            return this.renderDisplay();
+        }
+    }
+});
+
+var Board = React.createClass({
+    propTypes: {
+        count: function(props, propName) {
+            if (typeof props[propName] !== "number"){
+                return new Error('The count property must be a number');
+            }
+            if (props[propName] > 50) {
+                return new Error("You have reached the maximum number " + props[propName] + "of appointments");
+            }
+        }
+    },
+    getInitialState: function() {
+        return {
+            notes: [
+                'Dentist Appointment at 9:00 a.m.',
+                'Doctor\'s Appointment at 1:00 p.m. ',
+                'Hair Cut Appointmet at 2:00 p.m.',
+                'Appointment with Plumber at 3:30 p.m.'
+            ]
+        };
+    },
+    update: function(newText, i) {
+        
+        var arr = this.state.notes;
+        arr[i] = newText;
+        this.setState({notes:arr});        
+     
+    },
+    remove: function(i) {
+        var arr = this.state.notes;
+        arr.splice(i, 1);
+        this.setState({notes: arr});       
+    },
+    eachNote: function(note, i) {
+        return (
+                <Note key={i}
+                    index={i}
+                    onChange={this.update}
+                    onRemove={this.remove}
+                >{note}</Note>
+            );
+},
+render: function() {
+    return (<div className="board">
+                {this.state.notes.map(this.eachNote)}
+        </div>
+
+    );
+}
+});
+
+
+React.render(<Board count={10}/>, 
+    document.getElementById('react-container'));
+
+
+
+
+
+
+
+
+
+
